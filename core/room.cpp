c0631ed85921277fbcd77945930c658f3c6f6536
Connor Hewitt <connor.hewitt@gmail.com>
2018-05-11 22:41:00 -0700

    Add room exits!
    Closes #1

--- core/room.cpp
+++ core/room.cpp
@@ -1,8 +1,20 @@
 #include "room.h"
 
+#include <iostream>
+
+template <class T>
+bool operator==(const std::weak_ptr<T> weak, std::nullptr_t);
+
+template <class T>
+bool operator==(const std::weak_ptr<T> weak, std::nullptr_t) {
+    using wt = std::weak_ptr<T>;
+    return !weak.owner_before(wt{}) && !wt{}.owner_before(weak);
+}
+
 Room::Room(Driver& _driver)
     : driver(_driver)
     , creatures()
+    , exits()
 {
 
 }
@@ -16,4 +28,12 @@ void Room::addCreature(std::shared_ptr<Creature> creature) {
 }
 void Room::removeCreature(std::shared_ptr<Creature> creature) {
     this->creatures.erase(creature);
+}
+
+void Room::addExit(std::string name, std::shared_ptr<Room> room) {
+    this->exits[name] = std::weak_ptr<Room>(room);
+}
+
+std::shared_ptr<Room> Room::getExit(std::string name) {
+    return (this->exits[name] == nullptr) ? nullptr : this->exits[name].lock();
 }
--- core/room.h
+++ core/room.h
@@ -2,9 +2,14 @@
 
 #include <unordered_set>
 #include <memory>
+#include <unordered_map>
+#include <iostream>
+
+#include "driver.h"
 
 class Creature;
 class Driver;
+class Room;
 
 class Room {
     public:
@@ -12,7 +17,12 @@ class Room {
         const std::unordered_set<std::shared_ptr<Creature>>& getCreatures();
         void addCreature(std::shared_ptr<Creature> creature);
         void removeCreature(std::shared_ptr<Creature> creature);
+
+        void addExit(std::string name, std::shared_ptr<Room> room);
+        std::shared_ptr<Room> getExit(std::string name);
     protected:
         Driver& driver;
         std::unordered_set<std::shared_ptr<Creature>> creatures;
+
+        std::unordered_map<std::string, std::weak_ptr<Room>> exits;
 };

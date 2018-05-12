e48083fe571da0d588e136737a177aabbf37d4bf
Connor Hewitt <connor.hewitt@gmail.com>
2018-05-11 22:38:20 -0700

    Rename backflip command parameters, because they have to be globally unique :/

--- core/commands/backflip_command.h
+++ core/commands/backflip_command.h
@@ -3,9 +3,9 @@
 
 #include "../simple_command.h"
 
-std::string name = "backflip";
+std::string backflipCommandName = "backflip";
 typedef std::tuple<std::string> StringTuple;
-Parameterizer<StringTuple> adverbParameterizer = [](Creature& actor, CommandWordIterator commandParameters) {
+Parameterizer<StringTuple> backflipAdverbParameterizer = [](Creature& actor, CommandWordIterator commandParameters) {
     (void)actor;
     if(commandParameters == CommandWordIterator()) {
         return OptionalParameterTuple<StringTuple>();
@@ -20,25 +20,25 @@ Parameterizer<StringTuple> adverbParameterizer = [](Creature& actor, CommandWord
     }
     return OptionalParameterTuple<StringTuple>(std::make_tuple<std::string>(out.str()));
 };
-Executor<StringTuple> adverbExecutor = [](Creature& actor, StringTuple&& tuple) {
+Executor<StringTuple> backflipAdverbExecutor = [](Creature& actor, StringTuple&& tuple) {
     (void)actor;
     std::cout << "You backflip " << std::get<0>(tuple) << "!" << std::endl;
 };
 
 
 typedef std::tuple<> EmptyTuple;
-Parameterizer<EmptyTuple> plainParameterizer = [](Creature& actor, CommandWordIterator commandParameters) -> OptionalParameterTuple<EmptyTuple> {
+Parameterizer<EmptyTuple> backflipPlainParameterizer = [](Creature& actor, CommandWordIterator commandParameters) -> OptionalParameterTuple<EmptyTuple> {
     (void)actor;
     if(commandParameters != CommandWordIterator()) {
         return OptionalParameterTuple<EmptyTuple>();
     }
     return OptionalParameterTuple<EmptyTuple>(std::make_tuple<>());
 };
-Executor<EmptyTuple> plainExecutor = [](Creature& actor, EmptyTuple&& tuple) {
+Executor<EmptyTuple> backflipPlainExecutor = [](Creature& actor, EmptyTuple&& tuple) {
     (void)tuple;
     (void)actor;
     std::cout << "You backflip!" << std::endl;
 };
 
-class BackflipAdverbCommand : public SimpleCommand<name, StringTuple, adverbParameterizer, adverbExecutor> { };
-class BackflipPlainCommand : public SimpleCommand<name, EmptyTuple, plainParameterizer, plainExecutor> { };
+class BackflipAdverbCommand : public SimpleCommand<backflipCommandName, StringTuple, backflipAdverbParameterizer, backflipAdverbExecutor> { };
+class BackflipPlainCommand : public SimpleCommand<backflipCommandName, EmptyTuple, backflipPlainParameterizer, backflipPlainExecutor> { };

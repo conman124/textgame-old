d4bd26eaa71df8215e43eb7547b71c7d1df9d9a7
Connor Hewitt <connor.hewitt@gmail.com>
2018-10-03 14:16:58 -0600

    Change driver to use threads for command queueing.

--- core/driver.cpp
+++ core/driver.cpp
@@ -15,12 +15,8 @@
 
 Driver::Driver() 
     : roomMaintainer()
-    , running(true)
     , player(std::make_shared<Player>(*this))
     , coreCommands(CoreCommandProvider().getCommands())
-    , commandsMutex()
-    , commands()
-    , heartbeatThread(beginHeartbeat())
 {
     std::shared_ptr<Room> rooms[3][3];
 	for(int i = 0; i < 3; i++) {
@@ -45,28 +41,21 @@ Driver::Driver()
 			this->roomMaintainer.visit(rooms[i][j]);
 		}
 	}
-    
+
     this->player->moveToRoom(rooms[1][1]);
 }
 
-Driver::~Driver() {
-    this->running = false;
-    this->heartbeatThread.join();
-}
-
-std::thread Driver::beginHeartbeat() {
-    return std::thread([this]() {
-        while(this->running) {
-            usleep(HEARTBEAT_US);
-            
-            this->heartbeat();
-        }
-    });
-}
-
 void Driver::queueCommand(std::string command) {
-    std::lock_guard<std::mutex> lock(this->commandsMutex);
-    this->commands.push(command);
+	if(command.length() == 0) { return; }
+
+	std::cout << "Command: " << command << std::endl;
+
+	std::list<std::unique_ptr<BoundCommand>> boundCommands = this->attemptCommandResolution(this->player, command);
+	if(boundCommands.size() != 1) {
+		std::cout << "I'm not sure what you mean..." << std::endl;
+	} else {
+		boundCommands.front()->execute();
+	}
 }
 
 std::list<std::unique_ptr<BoundCommand>> Driver::attemptCommandResolution(std::shared_ptr<Creature> actor, std::string command) {
@@ -77,23 +66,4 @@ std::list<std::unique_ptr<BoundCommand>> Driver::attemptCommandResolution(std::s
         boundCommands.splice(boundCommands.end(), someBoundCommands, someBoundCommands.begin(), someBoundCommands.end());
     }
     return boundCommands;
-}
-
-void Driver::heartbeat() {
-    std::lock_guard<std::mutex> lock(this->commandsMutex);
-
-    while(this->commands.size() > 0) {
-        auto command = this->commands.front();
-        this->commands.pop();
-        if(command.length() == 0) { continue; }
-
-        std::cout << "Command: " << command << std::endl;
-
-        std::list<std::unique_ptr<BoundCommand>> boundCommands = this->attemptCommandResolution(this->player, command);
-        if(boundCommands.size() != 1) {
-            std::cout << "I'm not sure what you mean..." << std::endl;
-        } else {
-            boundCommands.front()->execute();
-        }
-    }
 }
--- core/driver.h
+++ core/driver.h
@@ -10,33 +10,24 @@
 #include <list>
 
 #include "room_maintainer.h"
+#include "unbound_command.h"
 
 class Creature;
 class Player;
-class UnboundCommand;
 class BoundCommand;
 
 class Driver {
     public:
         Driver();
-        ~Driver();
+        ~Driver() {};
 
         void queueCommand(std::string command);
     private:
         RoomMaintainer roomMaintainer;
 
-        bool running;
-
         std::shared_ptr<Player> player;
 
         std::list<std::unique_ptr<UnboundCommand>> coreCommands;
 
-        std::mutex commandsMutex;
-        std::queue<std::string> commands;
-
         std::list<std::unique_ptr<BoundCommand>> attemptCommandResolution(std::shared_ptr<Creature> actor, std::string command);
-
-        std::thread beginHeartbeat();
-        std::thread heartbeatThread;
-        void heartbeat();
 };

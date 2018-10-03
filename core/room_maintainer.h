7210f09c67e481e61bee71eb2c24c62deac4cf41
Connor Hewitt <connor.hewitt@gmail.com>
2018-10-03 11:33:22 -0600

    Add comment describing RoomMaintainer

--- core/room_maintainer.h
+++ core/room_maintainer.h
@@ -1,8 +1,17 @@
+#pragma once
+
 #include <unordered_set>
 #include <memory>
 
 class Room;
 
+/**
+ * This is not currently used for much, but eventually, it will
+ * be in charge of maintaining a how recently each room was visited,
+ * and freeing rooms when they haven't been visited for a while.
+ * This will reduce memory usage, while still making it so that objects
+ * and NPCs in a certain room will stay for a while.
+ */
 class RoomMaintainer {
     public:
         RoomMaintainer();

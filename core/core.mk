54b770160a266349437b951a681537491496d361
Connor Hewitt <connor.hewitt@gmail.com>
2018-10-03 22:17:44 -0600

    Remove unnecessary pthread library

--- core/core.mk
+++ core/core.mk
@@ -1,6 +1,6 @@
 DIR=core/
 
-$(BINDIR)game: LDLIBS=-lpthread -lcommands -litems
+$(BINDIR)game: LDLIBS=-lcommands -litems
 $(BINDIR)game: $(patsubst $(DIR)%.cpp,$(BUILDDIR)$(DIR)%.o,$(wildcard $(DIR)*.cpp)) $(BUILDDIR)core/commands/libcommands.a $(BUILDDIR)core/items/libitems.a
 	@mkdir -p $(@D)
 	$(CXX) $(LDFLAGS) $^ -o $@ $(LOADLIBES) $(LDLIBS)

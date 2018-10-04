9ff9a716fd1deeb9f4a2a417bcea5312a7aec8b3
Connor Hewitt <connor.hewitt@gmail.com>
2018-10-03 20:57:11 -0600

    Add item grouping to ItemContainer's describe method.

--- core/item_container.cpp
+++ core/item_container.cpp
@@ -3,6 +3,10 @@
 #include <memory>
 #include <string>
 #include <sstream>
+#include <typeindex>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
 
 #include "item.h"
 #include "item_container.h"
@@ -15,32 +19,53 @@ ItemContainer::ItemContainer()
 
 std::string ItemContainer::describe() {
 	std::ostringstream os;
-	if(this->items.size() == 0) {
-		return "";
-	} else if(this->items.size() == 1) {
-		os << "There is " << this->items.front()->getName() << " here.";
-	} else if(this->items.size() == 2) {
-		auto it = this->items.begin();
-		os << "There are ";
-		os << (*it)->getName();
-		os << " and ";
-		it++;
-		os << (*it)->getName();
-		os << " here.";
-	} else {
-		auto it = this->items.begin();
-		os << "There are ";
-		for(size_t i = 0; i < this->items.size(); i++) {
-			if(i == this->items.size() - 1) {
-				os << "and ";
+	auto counts = this->getGroupedItemCounts();
+
+	switch(counts.size()) {
+		case 0: return "";
+		case 1: {
+			os << "There ";
+			if(counts.begin()->second > 1) {
+				os << "are ";
+			} else {
+				os << "is ";
 			}
-			os << (*it)->getName();
-			if(i != this->items.size() - 1) {
-				os << ", ";
-			}
-			it++;
+			auto item = this->items.front();
+			os << item->getPluralNameWithCount(counts[typeid(*item)]) << " here.";
+			break;
 		}
-		os << " here.";
+		case 2: {
+			auto it = this->items.begin();
+			std::type_index firstType(typeid(**it));
+
+			os << "There are ";
+			os << (*it)->getPluralNameWithCount(counts[typeid(**it)]);
+			os << " and ";
+			do {
+				it++;
+			} while(firstType == std::type_index(typeid(**it)));
+			os << (*it)->getPluralNameWithCount(counts[typeid(**it)]);
+			os << " here.";
+			break;
+		}
+		default: {
+			std::unordered_set<std::type_index> seen;
+			auto it = this->items.begin();
+			os << "There are ";
+			for(size_t i = 0; i < counts.size(); i++) {
+				if(i == counts.size() - 1) {
+					os << "and ";
+				}
+				os << (*it)->getPluralNameWithCount(counts[typeid(**it)]);
+				if(i != counts.size() - 1) {
+					os << ", ";
+				}
+				do {
+					it++;
+				} while(seen.find(std::type_index(typeid(**it))) != seen.end());
+			}
+			os << " here.";
+		}
 	}
 
 	return os.str();
@@ -48,4 +73,18 @@ std::string ItemContainer::describe() {
 
 void ItemContainer::addItem(std::shared_ptr<Item> item) {
 	this->items.push_back(item);
+}
+
+std::unordered_map<std::type_index, size_t> ItemContainer::getGroupedItemCounts() {
+	std::unordered_map<std::type_index, size_t> counts;
+
+	for(auto item : this->items) {
+		std::type_index index(typeid(*item));
+		if(counts.find(index) == counts.end()) {
+			counts[index] = 0;
+		}
+		counts[index]++;
+	}
+
+	return counts;
 }
--- core/item_container.h
+++ core/item_container.h
@@ -2,6 +2,8 @@
 
 #include <list>
 #include <memory>
+#include <unordered_map>
+#include <typeindex>
 
 #include "item.h"
 #include "traits/describable.h"
@@ -11,7 +13,8 @@ class ItemContainer : public Describable {
 		ItemContainer();
 		std::string describe();
 		void addItem(std::shared_ptr<Item> item);
-
 	private:
 		std::list<std::shared_ptr<Item>> items;
+
+		std::unordered_map<std::type_index, size_t> getGroupedItemCounts();
 };
